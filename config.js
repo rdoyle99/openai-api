d1db135b5cf678ab4f05eb0c2718d68b18e9c308
rdoyle99 <rpdoyle1@gmail.com>
2021-08-15 11:45:40 -0400

    engine change'

--- config.js
+++ config.js
@@ -1,8 +1,17 @@
-const DEFAULT_ENGINE = 'davinci';
-const ENGINE_LIST = ['ada', 'babbage', 'curie', 'davinci', 'davinci-instruct-beta', 'curie-instruct-beta', 'content-filter-alpha-c4'];
-const ORIGIN = 'https://api.openai.com';
-const API_VERSION = 'v1';
-const OPEN_AI_URL = `${ORIGIN}/${API_VERSION}`
+const DEFAULT_ENGINE = "davinci";
+const ENGINE_LIST = [
+  "ada",
+  "babbage",
+  "curie",
+  "davinci",
+  "davinci-instruct-beta",
+  "curie-instruct-beta",
+  "content-filter-alpha-c4",
+  "curie:ft-magic-sales-bot-2021-08-12-21-54-55",
+];
+const ORIGIN = "https://api.openai.com";
+const API_VERSION = "v1";
+const OPEN_AI_URL = `${ORIGIN}/${API_VERSION}`;
 
 module.exports = {
   completionURL(engine) {
@@ -24,12 +33,12 @@ module.exports = {
     return `${OPEN_AI_URL}/engines/${engine}`;
   },
   classificationsUrl() {
-    return `${OPEN_AI_URL}/classifications`
+    return `${OPEN_AI_URL}/classifications`;
   },
   filesUrl() {
-    return `${OPEN_AI_URL}/files`
+    return `${OPEN_AI_URL}/files`;
   },
   answersUrl() {
-    return `${OPEN_AI_URL}/answers`
-  }
+    return `${OPEN_AI_URL}/answers`;
+  },
 };
